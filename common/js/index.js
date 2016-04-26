98720916761d3b03feabe40a0cfe8e9feafbfa24
Narendra <narendra.nitm@gmail.com>
2016-04-26 17:30:42 +0530

    initial commit

---
+++ common/js/index.js
@@ -0,0 +1,39 @@
+ $(document).ready(function () {
+	 "use strict";
+	$('.img_container .box').hover(function(){
+		$(this).toggleClass('active');	
+	});
+	
+	$('.toggle').click(function(){
+		$('.left_container').toggleClass("more");	
+		$('.right_container').toggleClass("less");	
+	});
+	$('.overlay').click(function(){
+		$('.left_container').removeClass("more");	
+		$('.right_container').removeClass("less");	
+	});
+	
+	$('.overlay, .toggle').click(function(){
+		$('.head-sec').toggleClass('sort');
+	});
+	//$('.categorybox input:radio').jqTransRadio();
+	});
+	
+	$(function($){
+		"use strict";
+		var $container = $('.boxout');	
+		$container.imagesLoaded(function(){
+			$container.masonry({
+			itemSelector: '.boxout .box',
+			columnWidth: 10,
+			isAnimated:true,
+			animationOptions: {
+			duration: 500,
+			easing:'linear',
+			queue :false
+			}
+			});
+		});	
+		});
+
+	
